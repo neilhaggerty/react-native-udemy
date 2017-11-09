c253b4a4dbada5587b69d49750586d3744112ffd
Neil Haggerty <neil.haggerty@mergermarket.com>
2017-11-09 11:18:18 +0000

    Finished 49

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -31,11 +32,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{ artist }</Text>
         </View>
       </CardSection>
+      
       <CardSection>
         <Image 
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Click Me</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
